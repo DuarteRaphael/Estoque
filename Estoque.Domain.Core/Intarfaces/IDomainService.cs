9fcfdda8d23cb77078c08dc430b1b05891bbe8c2
Raphael Duarte <raphael-duarte@outlook.com>
2024-06-17 05:42:20 -0300

    Adicionar arquivos de projeto.

---
+++ Estoque.Domain.Core/Intarfaces/IDomainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque.Domain.Core.Intarfaces
+{
+    public interface IDomainService<TEntity, TKey> : IDisposable
+        where TEntity : class
+    {
+        Task CadastrarAsync(TEntity entity);
+        Task AtualizarAsynk(TEntity entity);
+        Task ExcluirAsynk(TEntity entity);
+        Task<List<TEntity>> BuscarTodos(int page, int limit);
+        Task<TEntity> BuscarPorId(TKey id);
+    }
+}
---
+++ Estoque.Services.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estoque.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Cadastrar()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Atualizar()
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Excluir()
+        {
+            return Ok();
+        }
+        [HttpGet("{page}/{limit}")]
+        public IActionResult BuscarTodos(int page, int limit)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult BusccarPorId(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
